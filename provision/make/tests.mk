993cb7474e4b14daac47f0fc756722afc8d864be
@slovacus <slovacus@gmail.com>
2020-05-10 22:15:21 -0500

    chore: implement actions for keybase (#25)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -1,22 +1,20 @@
 test.help:
 	@echo '    Tests:'
 	@echo ''
-	@echo '        test                    Run all test'
-	@echo '        test.run                Run test of test'
-	@echo '        test.lint               Run all pre-commit'
-	@echo '        test.syntax             Run all syntax in code'
+	@echo '        test                      show help'
+	@echo '        test.all   	             Run all module test'
+	@echo '        test.lint                 Run all pre-commit'
 	@echo ''
 
 test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
 	@if [ -z "${run}" ]; then \
-		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest"; \
-	elif [ -n "${run}" ]; then \
-		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest tests/${run}"; \
+		make test.help;\
 	fi
+
+test.all:
+	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
+	$(docker-test-run) bash -c "zunit --verbose"
 
 test.lint:
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
-
-test.syntax:
-	@echo $(MESSAGE) Running tests $(END)
