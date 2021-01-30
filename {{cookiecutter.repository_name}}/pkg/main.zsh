06f3787f3129fd05a027e719b25cf39aafd6609f
@slovacus <slovacus@gmail.com>
2021-01-29 23:03:48 -0500

    style: code template cookiecutter zsh (#3)

--- {{cookiecutter.repository_name}}/pkg/main.zsh
+++ {{cookiecutter.repository_name}}/pkg/main.zsh
@@ -20,7 +20,6 @@ function {{cookiecutter.package_name}}::pkg::main::factory {
 
     # shellcheck source=/dev/null
     source {{ "${ZSH_" }}{% filter upper %}{{cookiecutter.package_name}}{% endfilter %}{{ "_PATH}" }}/pkg/helper.zsh
-
 }
 
 {{cookiecutter.package_name}}::pkg::main::factory
